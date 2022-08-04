c93156daae551e793c48401617e27b52b5cc2f1a
Daniel Vidales <daniel.vidales@dexcom.com>
2022-08-03 17:17:44 -0700

    Content-Provider. Adds NoteKeperProvider:ContentProvider and corresponding AndroidManifest.xml declaration

---
+++ app/src/main/java/com/dexcom/sdk/aac_fullcontentapp/provider/NoteKeeperProvider.kt
@@ -0,0 +1,47 @@
+package com.dexcom.sdk.aac_fullcontentapp.provider
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
+import com.dexcom.sdk.aac_fullcontentapp.database.NoteKeeperDatabaseContract.*
+import com.dexcom.sdk.aac_fullcontentapp.database.NoteKeeperOpenHelper
+class NoteKeeperProvider : ContentProvider() {
+
+    var dbOpenHelper:NoteKeeperOpenHelper? =  null
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
+        TODO("Implement this to handle requests to delete one or more rows")
+    }
+
+    override fun getType(uri: Uri): String? {
+        TODO(
+            "Implement this to handle requests for the MIME type of the data" +
+                    "at the given URI"
+        )
+    }
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        TODO("Implement this to handle requests to insert a new row.")
+    }
+
+    override fun onCreate(): Boolean {
+        return false
+    }
+
+
+    override fun query(
+        uri: Uri, projection: Array<String>?, selection: String?,
+        selectionArgs: Array<String>?, sortOrder: String?
+    ): Cursor? {
+        val db = dbOpenHelper?.readableDatabase
+        var cursor = db?.query(CourseInfoEntry.TABLE_NAME, projection, selection, selectionArgs, null, null, sortOrder)
+        return cursor
+    }
+
+    override fun update(
+        uri: Uri, values: ContentValues?, selection: String?,
+        selectionArgs: Array<String>?
+    ): Int {
+        TODO("Implement this to handle requests to update one or more rows.")
+    }
+}

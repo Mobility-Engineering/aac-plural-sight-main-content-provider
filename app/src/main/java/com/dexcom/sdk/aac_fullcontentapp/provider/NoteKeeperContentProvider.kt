588be99630de427036b29dd867fa0694442b27e3
Daniel Vidales <daniel.vidales@dexcom.com>
2022-08-11 16:54:17 -0700

    CONTENT_PROVIDER. overrides insert() and delete() invoked from ContentProviderClient implemented in ViewModel

--- app/src/main/java/com/dexcom/sdk/aac_fullcontentapp/provider/NoteKeeperContentProvider.kt
+++ app/src/main/java/com/dexcom/sdk/aac_fullcontentapp/provider/NoteKeeperContentProvider.kt
@@ -1,11 +1,15 @@
 package com.dexcom.sdk.aac_fullcontentapp.provider
 
+ import android.app.UiAutomation
 import android.content.ContentProvider
+import android.content.ContentUris
 import android.content.ContentValues
 import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
+import android.provider.BaseColumns
 import com.dexcom.sdk.aac_fullcontentapp.database.NoteKeeperDatabaseContract.*
+
 import com.dexcom.sdk.aac_fullcontentapp.database.NoteKeeperOpenHelper
 import kotlinx.coroutines.selects.select
 
@@ -13,6 +17,7 @@ class NoteKeeperContentProvider : ContentProvider() {
 
     private val COURSES = 0
     private val NOTES = 1
+    private val NOTES_ROW = 2
     private val NOTES_EXPANDED = 2
     var dbOpenHelper: NoteKeeperOpenHelper? = null
     val uriMatcher = UriMatcher(UriMatcher.NO_MATCH)
@@ -28,71 +33,118 @@ class NoteKeeperContentProvider : ContentProvider() {
             NoteKeeperProviderContract.Notes.PATH,
             NOTES
         )
-    }
-
-    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
-        TODO("Implement this to handle requests to delete one or more rows")
-    }
-
-    override fun getType(uri: Uri): String? {
-        TODO(
-            "Implement this to handle requests for the MIME type of the data" +
-                    "at the given URI"
+        uriMatcher.addURI(
+            NoteKeeperProviderContract.AUTHORITY,
+            "${NoteKeeperProviderContract.Notes.PATH}/#",
+            NOTES_ROW
         )
     }
 
-    override fun insert(uri: Uri, values: ContentValues?): Uri? {
-        TODO("Implement this to handle requests to insert a new row.")
-    }
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
 
-    override fun onCreate(): Boolean {
-        dbOpenHelper = NoteKeeperOpenHelper(context)
-        //context for Content provider
-        return true
-    }
-
-
-    override fun query(
-
-        uri: Uri, projection: Array<String>?, selection: String?,
-        selectionArgs: Array<String>?, sortOrder: String?
-    ): Cursor? {
+        val db = dbOpenHelper?.writableDatabase
         val uriMatch = uriMatcher.match(uri)
-        val db = dbOpenHelper?.readableDatabase
-        var cursor: Cursor? = null
         when (uriMatch) {
-            COURSES -> {
-                cursor = db?.query(
-                    CourseInfoEntry.TABLE_NAME,
-                    projection,
-                    selection,
-                    selectionArgs,
-                    null,
-                    null,
-                    sortOrder
-                )
+            NOTES_ROW -> {
+             db?.delete(NoteInfoEntry.TABLE_NAME, selection, selectionArgs)
             }
-            NOTES -> {
-                cursor = db?.query(
-                    NoteInfoEntry.TABLE_NAME,
-                    projection,
-                    selection,
-                    selectionArgs,
-                    null,
-                    null,
-                    sortOrder
-                )
-            }
-
         }
-        return cursor
-    }
+        return 1
+}
 
-    override fun update(
-        uri: Uri, values: ContentValues?, selection: String?,
-        selectionArgs: Array<String>?
-    ): Int {
-        TODO("Implement this to handle requests to update one or more rows.")
+override fun getType(uri: Uri): String? {
+    TODO(
+        "Implement this to handle requests for the MIME type of the data" +
+                "at the given URI"
+    )
+}
+
+override fun insert(uri: Uri, values: ContentValues?): Uri? {
+    val db = dbOpenHelper?.readableDatabase
+    var rowId: Long? = -1
+    var rowUri: Uri? = null
+    val uriMatch = uriMatcher.match(uri)
+    when (uriMatch) {
+        NOTES -> {
+            rowId = db?.insert(NoteInfoEntry.TABLE_NAME, null, values)
+            rowUri =
+                ContentUris.withAppendedId(
+                    NoteKeeperProviderContract.Notes.CONTENT_URI,
+                    rowId ?: rowId ?: -1
+                )
+        }
+        COURSES -> {
+            rowId = db?.insert(CourseInfoEntry.TABLE_NAME, null, values)
+            rowUri = ContentUris.withAppendedId(
+                NoteKeeperProviderContract.Courses.CONTENT_URI,
+                rowId ?: rowId ?: -1
+            )
+        }
     }
+    return rowUri
+}
+
+override fun onCreate(): Boolean {
+    dbOpenHelper = NoteKeeperOpenHelper(context)
+    //context for Content provider
+    return true
+}
+
+
+override fun query(
+
+    uri: Uri, projection: Array<String>?, selection: String?,
+    selectionArgs: Array<String>?, sortOrder: String?
+): Cursor? {
+    val uriMatch = uriMatcher.match(uri)
+    val db = dbOpenHelper?.readableDatabase
+    var cursor: Cursor? = null
+    when (uriMatch) {
+        COURSES -> {
+            cursor = db?.query(
+                CourseInfoEntry.TABLE_NAME,
+                projection,
+                selection,
+                selectionArgs,
+                null,
+                null,
+                sortOrder
+            )
+        }
+        NOTES -> {
+            cursor = db?.query(
+                NoteInfoEntry.TABLE_NAME,
+                projection,
+                selection,
+                selectionArgs,
+                null,
+                null,
+                sortOrder
+            )
+        }
+        NOTES_ROW -> {
+            val rowId = ContentUris.parseId(uri)
+            val rowSelection = "${BaseColumns._ID} =?"
+            val rowSelectionArgs = arrayOf(rowId.toString())
+            cursor = db?.query(
+                NoteInfoEntry.TABLE_NAME,
+                projection,
+                rowSelection,
+                rowSelectionArgs,
+                null,
+                null,
+                null
+            )
+        }
+    }
+    return cursor
+}
+
+override fun update(
+    uri: Uri, values: ContentValues?, selection: String?,
+    selectionArgs: Array<String>?
+): Int {
+    TODO("Implement this to handle requests to update one or more rows.")
+}
 
 }

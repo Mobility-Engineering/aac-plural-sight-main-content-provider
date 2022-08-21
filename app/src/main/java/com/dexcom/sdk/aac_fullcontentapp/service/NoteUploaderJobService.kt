cbc4346574c85103ac4b22ab2c637b13a7311980
Daniel Vidales <daniel.vidales@dexcom.com>
2022-08-21 06:48:05 -0700

    JOB_SCHEDULER. Adds necesarry modifications to its JobService that are needed to execute job on background thread via an Executor. Leaves comments on several resources for Threading that Android offers

--- app/src/main/java/com/dexcom/sdk/aac_fullcontentapp/service/NoteUploaderJobService.kt
+++ app/src/main/java/com/dexcom/sdk/aac_fullcontentapp/service/NoteUploaderJobService.kt
@@ -4,16 +4,30 @@ import android.app.job.JobParameters
 import android.app.job.JobService
 import android.content.Context
 import android.net.Uri
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import java.util.concurrent.Executors
 
-class NoteUploaderJobService(): JobService() {
+class NoteUploaderJobService() : JobService() {
     override fun onStartJob(jobParameters: JobParameters): Boolean {
         val stringDataUri = jobParameters.getExtras().getString(EXTRA_DATA_URI)
         val dataUri = Uri.parse(stringDataUri)
-        NoteUploader.doUpload(baseContext, dataUri)
-        if (!NoteUploader.isCanceled)
-        jobFinished(jobParameters, false)
-        return false
+        Executors.newSingleThreadExecutor().execute(kotlinx.coroutines.Runnable {
+            NoteUploader.doUpload(baseContext, dataUri)
+
+            if (NoteUploader.isCanceled)
+                jobFinished(jobParameters, false)
+
+        })
+            return false
     }
+
+
 
     override fun onStopJob(params: JobParameters?): Boolean {
         NoteUploader.cancel()
